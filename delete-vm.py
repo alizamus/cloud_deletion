7ff5a5c7fc0771653a1607e429b1c9cee1f9f690
Ali Zamani <areza@juniper.net>
2015-06-29 12:51:48 -0400

    fix the bugs and make it more complete

--- delete-vm.py
+++ delete-vm.py
@@ -1,10 +1,14 @@
-from keystoneclient.v2_0 import client
-keystone = client.Client(username='admin';, password='secret123', tenant_name='admin', auth_url="http://127.0.0.1:5000/v2.0")
+from keystoneclient.v2_0 import client as kclient
+from novaclient import client as nclient
+keystone = kclient.Client(username='admin', password='secret123', tenant_name='admin', auth_url="http://127.0.0.1:5000/v2.0")
 predef_tenants = ['admin','demo','service','invisible_to_admin']
 tenants = []
 tenans_list = keystone.tenants.list()
-for i in range(tenans_list):
-	if tenans_list[i] not in:
-		tenants.append(tenans_list[i])
-
+for i in range(len(tenans_list)):
+	if tenans_list[i].name not in predef_tenants:
+		tenants.append(str(tenans_list[i].name))
 print tenants
+
+nova = nclient.Client(2,'admin','secret123', 'ali', "http://127.0.0.1:5000/v2.0",service_type="compute")
+compute_list = nova.servers.list()
+print compute_list[0].name
