109775f3d56b8bd88693c91c209daf3f64797974
Ali Zamani <areza@juniper.net>
2015-06-29 22:06:59 -0400

    user deletion is done

--- delete-user.py
+++ delete-user.py
@@ -3,12 +3,19 @@ from novaclient import client as nclient
 keystone = kclient.Client(username='admin', password='secret123', tenant_name='admin', auth_url="http://127.0.0.1:5000/v2.0")
 predef_tenants = ['admin','demo','service','invisible_to_admin']
 tenants = []
+tenants_id = []
 tenans_list = keystone.tenants.list()
 for i in range(len(tenans_list)):
 	if tenans_list[i].name not in predef_tenants:
 		tenants.append(str(tenans_list[i].name))
+		tenants_id.append(str(tenans_list[i].id))
 
-keystone_user = kclient.Client(username='admin', password='secret123', tenant_name='ali', auth_url="http://127.0.0.1:5000/v2.0")
-
-tenants_list = keystone_user.users.list()
-print tenants_list
+predef_users = ['admin','demo','nova','heat','glance','cinder','neutron']
+for tenant in tenants_id:
+	keystone_user = kclient.Client(username='admin', password='secret123', tenant_id=tenant, auth_url="http://127.0.0.1:5000/v2.0")
+	#print keystone_user.users.list(tenant)
+	users_list = keystone_user.users.list(tenant)
+	if users_list:
+                for i in range(len(users_list)):
+			if users_list[i].username not in predef_users:
+				users_list[i].delete()
