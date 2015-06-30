662f286b21e20d4ab3871f27d18b0972ed5334d2
Ali Zamani <areza@juniper.net>
2015-06-30 15:10:43 -0400

    fix the bug in calling

--- delete-floating-ip-pool.py
+++ delete-floating-ip-pool.py
@@ -7,36 +7,41 @@ import os
 import csv
 import getopt
 
-try:
-        opts, args = getopt.getopt(argv, "ha:", ["help", "admin_pass="])
-except getopt.GetoptError:                                
-        sys.exit(2)
+def main(argv):
+	try:
+	        opts, args = getopt.getopt(argv, "ha:", ["help", "admin_pass="])
+	except getopt.GetoptError:                                
+	        sys.exit(2)
+	
+	for opt, arg in opts:       
+	        if opt in ("-h", "--help"):        
+	                sys.exit()        
+	        elif opt in ("-a", "--admin_pass"):
+	                admin_pass = arg
+	
+	keystone = kclient.Client(username='admin', password=admin_pass, tenant_name='admin', auth_url="http://127.0.0.1:5000/v2.0")
+	predef_tenants = ['admin','demo','service','invisible_to_admin']
+	tenants = []
+	tenants_id = []
+	tenans_list = keystone.tenants.list()
+	for i in range(len(tenans_list)):
+		if tenans_list[i].name not in predef_tenants:
+			tenants.append(str(tenans_list[i].name))
+			tenants_id.append(str(tenans_list[i].id))
+	
+	
+	for tenant in tenants:
+		res= os.system("./code/config --username admin --password " + admin_pass + " --tenant " + tenant +  " --api-server 127.0.0.1 show floating-ip-pool" + " > " + "test.csv")
+	
+		with open('./test.csv', 'rb') as csvfile:
+			rules = csv.reader(csvfile)
+			for row in rules:
+				a = str(row[0])
+				b = a.split(' in network ',1)[1]
+				c = a.split(' in network ',1)[0]
+				comand = "./code/config --username admin --password " + admin_pass + " --tenant " + tenant +  " --api-server 127.0.0.1 delete floating-ip-pool " + "--network " + c + " "  + "'" + b + "'"	
+				os.system(comand)
 
-for opt, arg in opts:       
-        if opt in ("-h", "--help"):        
-                sys.exit()        
-        elif opt in ("-a", "--admin_pass"):
-                admin_pass = arg
 
-keystone = kclient.Client(username='admin', password=admin_pass, tenant_name='admin', auth_url="http://127.0.0.1:5000/v2.0")
-predef_tenants = ['admin','demo','service','invisible_to_admin']
-tenants = []
-tenants_id = []
-tenans_list = keystone.tenants.list()
-for i in range(len(tenans_list)):
-	if tenans_list[i].name not in predef_tenants:
-		tenants.append(str(tenans_list[i].name))
-		tenants_id.append(str(tenans_list[i].id))
-
-
-for tenant in tenants:
-	res= os.system("./code/config --username admin --password " + admin_pass + " --tenant " + tenant +  " --api-server 127.0.0.1 show floating-ip-pool" + " > " + "test.csv")
-
-	with open('./test.csv', 'rb') as csvfile:
-		rules = csv.reader(csvfile)
-		for row in rules:
-			a = str(row[0])
-			b = a.split(' in network ',1)[1]
-			c = a.split(' in network ',1)[0]
-			comand = "./code/config --username admin --password " + admin_pass + " --tenant " + tenant +  " --api-server 127.0.0.1 delete floating-ip-pool " + "--network " + c + " "  + "'" + b + "'"	
-			os.system(comand)
+if __name__ == "__main__":
+    main(sys.argv[1:])
